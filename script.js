b2b06b9a916b9a7eab0b78a16bef11225462e386
Daniel Misi <misikalenga@gmail.com>
2025-03-19 00:06:41 +0100

    quiz termine

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
+// afficher et cacher les block
+
+let quiz = document.getElementById("quiz");
+let anki = document.getElementById("anki");
+let bQuiz = document.getElementById("blockQuiz");
+let bAnki = document.getElementById("blockAnki");
+
+
+quiz.addEventListener('click', function(){
+    bQuiz.style.display = "block";
+    bAnki.style.display = "none";
+})
+
+anki.addEventListener('click', function(){
+    bAnki.style.display = "block";
+    bQuiz.style.display = "none";
+})
+
 // Quiz
 
 let start = document.getElementById("start");
+let askQuiz = document.getElementById("askQuiz");
 let quizStart = document.getElementById("quizStart");
 let rep1 = document.getElementById("rep1");
 let rep2 = document.getElementById("rep2");
 let rep3 = document.getElementById("rep3");
 let rep4 = document.getElementById("rep4");
+let notesQ = document.getElementById("notesQ");
 
 let scoreQ = 0;
 let totalQ = 0;
@@ -26,6 +46,7 @@ start.addEventListener('click', function(){
 
 // Quiz question
 
+
 let repQuiz = Object.entries(etude);
 let piegeQuiz1 = Object.entries(etude);
 let piegeQuiz2 = Object.entries(etude);
@@ -35,8 +56,33 @@ let indexQP1 = 0;
 let indexQP2 = 0;
 let indexQP3 = 0;
 
+let [cleRep, valeurRep] = repQuiz[indexQ];
+let [clePi1, valeurPi1] = repQuiz[indexQP1];
+let [clePi2, valeurPi2] = repQuiz[indexQP2];
+let [clePi3, valeurPi3] = repQuiz[indexQP3];
+
+let repList= [];
+
+let a = 0;
+let b = 0;
+let c = 0;
+let d = 0;
 
 function quizAsk(){
+    
+    bQuiz.style.background = "rgb(255, 255, 255)";
+
+    indexQ = 0;
+    indexQP1 = 0;
+    indexQP2 = 0;
+    indexQP3 = 0;
+
+    a = 0;
+    b = 0;
+    c = 0;
+    d = 0;
+
+
 
     while (indexQ === indexQP1 || indexQ === indexQP2 || indexQ === indexQP3 || indexQP1 === indexQP2 || indexQP1 === indexQP3 || indexQP2 === indexQP3 ){
         indexQ = Math.floor(Math.random() * repQuiz.length);
@@ -44,9 +90,100 @@ function quizAsk(){
         indexQP2 = Math.floor(Math.random() * repQuiz.length);
         indexQP3 = Math.floor(Math.random() * repQuiz.length);
     }
-    console.log(indexQ, indexQP1, indexQP2, indexQP3)
+    
+    [cleRep, valeurRep] = repQuiz[indexQ];
+    [clePi1, valeurPi1] = repQuiz[indexQP1];
+    [clePi2, valeurPi2] = repQuiz[indexQP2];
+    [clePi3, valeurPi3] = repQuiz[indexQP3];
+
+    askQuiz.innerHTML = '<p>' + cleRep + '</p>';
+
+    repList = [valeurRep, valeurPi1, valeurPi2, valeurPi3];
+
+    while (a === b || a === c || a === d || b === c || b === d || c === d ){
+    a = Math.floor(Math.random() * repList.length);
+    b = Math.floor(Math.random() * repList.length);
+    c = Math.floor(Math.random() * repList.length);
+    d = Math.floor(Math.random() * repList.length);
+    }
+
+    rep1.innerHTML = repList[a];
+    rep2.innerHTML = repList[b];
+    rep3.innerHTML = repList[c];
+    rep4.innerHTML = repList[d];
+
 }
-quizAsk()
+quizAsk();
+
+
+rep1.addEventListener('click', function(){
+    if (repList[a]=== valeurRep){
+        rep1.innerHTML = "Bonne réponse";
+        scoreQ += 1;
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        bQuiz.style.background = "rgb(121, 248, 178)";
+        setTimeout(quizAsk, 1000);
+    }else{
+        rep1.innerHTML = "Mauvaise réponse";
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        setTimeout(quizAsk, 1000);
+        bQuiz.style.background = "rgb(248, 121, 121)";
+    }
+    
+})
+rep2.addEventListener('click', function(){
+    if (repList[b] === valeurRep){
+        rep2.innerHTML = "Bonne réponse";
+        scoreQ += 1;
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        bQuiz.style.background = "rgb(121, 248, 178)";
+        setTimeout(quizAsk, 1000);
+    }else{
+        rep2.innerHTML = "Mauvaise réponse";
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        setTimeout(quizAsk, 1000);
+        bQuiz.style.background = "rgb(248, 121, 121)";
+    }
+    
+})
+rep3.addEventListener('click', function(){
+    if (repList[c] === valeurRep){
+        rep3.innerHTML = "Bonne réponse";
+        scoreQ += 1;
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        bQuiz.style.background = "rgb(121, 248, 178)";
+        setTimeout(quizAsk, 1000);
+    }else{
+        rep3.innerHTML = "Mauvaise réponse";
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        setTimeout(quizAsk, 1000);
+        bQuiz.style.background = "rgb(248, 121, 121)";
+    }
+    
+})
+rep4.addEventListener('click', function(){
+    if (repList[d] === valeurRep){
+        rep4.innerHTML = "Bonne réponse";
+        scoreQ += 1;
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        bQuiz.style.background = "rgb(121, 248, 178)";
+        setTimeout(quizAsk, 1000);
+    }else{
+        rep4.innerHTML = "Mauvaise réponse";
+        totalQ += 1;
+        notesQ.innerHTML = "<p>" + scoreQ + "/" + totalQ + "</p>";
+        setTimeout(quizAsk, 1000);
+        bQuiz.style.background = "rgb(248, 121, 121)";
+    }
+    
+})
 
 
 // CARTE
@@ -101,20 +238,4 @@ oui.addEventListener('click', function(){
 
 ankiAsk();
 
-// afficher et cacher les block
 
-let quiz = document.getElementById("quiz");
-let anki = document.getElementById("anki");
-let bQuiz = document.getElementById("blockQuiz");
-let bAnki = document.getElementById("blockAnki");
-
-
-quiz.addEventListener('click', function(){
-    bQuiz.style.display = "block";
-    bAnki.style.display = "none";
-})
-
-anki.addEventListener('click', function(){
-    bAnki.style.display = "block";
-    bQuiz.style.display = "none";
-})
